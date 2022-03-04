037be9a3150660ff1057e1e15ea0da5a851baf1c
Ivan <ivancammarota@bbcag,ch>
2022-03-04 11:18:16 +0100

    A very good commit message

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,13 @@
 import test from "./mylib"
+import data from "./../../assets/data/stuff.json"
 
+console.log(data)
 
 function wait(millis) {
     return new Promise((resolve) => {
         setTimeout(resolve, millis)
     })
 }
-
 
 async function main() {
     console.log("Hello...")
@@ -15,5 +16,33 @@ async function main() {
 }
 
 main()
-
 test()
+
+const menuButton = document.getElementById("menu")
+const nav = document.getElementById("nav")
+
+let menuIsOpen = false
+
+menuButton.addEventListener("click", () => {
+    menuIsOpen = !menuIsOpen
+
+    if(menuIsOpen) {
+        menuButton.innerText = "✖"
+        nav.classList.add("visible")
+    } else {
+        menuButton.innerText = "☰"
+        nav.classList.remove("visible")
+    }
+})
+
+let currentTheme = "light"
+
+document.getElementById("mode-switch").addEventListener("click", () => {
+    if(currentTheme === "light") {
+        currentTheme = "dark"
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        currentTheme = "light"
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+})
